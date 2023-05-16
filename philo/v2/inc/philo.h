499173c84ef395e925d9b3344fe0bcccce7681b7
Alexandre Couture <acouture@c3r4p5.42quebec.com>
2023-05-16 14:37:31 -0400

    updated code

---
+++ philo/v2/inc/philo.h
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 16:21:21 by acouture          #+#    #+#             */
+/*   Updated: 2023/05/10 16:26:45 by acouture         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+# define FORK_TAKEN "has taken a fork"
+# define PHILO_EATING "is eating"
+# define PHILO_SLEEPING "is sleeping"
+# define PHILO_THINKING "is thinking"
+# define PHILO_DEAD "died"
+
+typedef struct s_philo
+{
+	pthread_t		thread_id;
+	pthread_t		death_thread;
+	int				philo_id;
+	int				nb_eat;
+	uint64_t		time_last_meal;
+
+}					t_philo;
+
+typedef struct s_mutex
+{
+	pthread_mutex_t	fork[200];
+	pthread_mutex_t	death_mutex;
+	pthread_mutex_t	change_state;
+	pthread_mutex_t	last_meal;
+	pthread_mutex_t	print;
+	pthread_mutex_t	eat;
+	pthread_mutex_t	check_death;
+}					t_mutex;
+
+typedef struct s_data
+{
+	t_mutex			mutex;
+	t_philo			philo[200];
+	bool			dead;
+	bool			full;
+	int				nb_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	uint64_t		start_time;
+}					t_data;
+
+// MAIN
+t_data				*call_struct(void);
+void				destroy_mutex(void);
+
+// UTILS
+void				my_sleep(uint64_t time);
+int					check_av(char **av);
+int					ft_atoi(char *s);
+uint64_t			time_stamp(void);
+int					print_action(int philo_id, uint64_t stamp, char *action);
+
+// INIT
+int					init_all(char **av);
+int					init_mutex(void);
+void				init_philo(void);
+void				init_args(char **av);
+
+// THREADS
+void				launcher(void);
+void				wait_thread(void);
+void				*routine(void *param);
+int					check_death(void);
+int					check_full(void);
+
+// ACTIONS
+void				philo_fork(t_philo *philo);
+void				philo_sleeping(t_philo *philo);
+#endif
---
+++ philo/v2/src/thread.c
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 17:15:03 by acouture          #+#    #+#             */
+/*   Updated: 2023/05/10 16:40:48 by acouture         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+/**
+ * The routine being executed by each philosopher
+ * Eating, sleeping, thinking
+*/
+void	*routine(void *param)
+{
+	t_philo	*philo;
+	t_data	*data;
+
+	philo = (t_philo *)param;
+	data = call_struct();
+	if (philo->philo_id % 2 == 0)
+		usleep(data->time_to_eat * 500);
+	while (check_death() == 0 && !data->full)
+	{
+		if (check_full() == 1)
+			return (NULL);
+		philo_fork(philo);
+		if (check_death() != 1)
+			philo_sleeping(philo);
+		print_action(philo->philo_id, time_stamp(), PHILO_THINKING);
+	}
+	return (NULL);
+}
+
+/**
+ * If arg[5] is present,
+	checks if every philo as eaten for args[5] amount of time
+*/
+int	check_full(void)
+{
+	t_data	*data;
+	t_philo	*philo;
+
+	data = call_struct();
+	philo = &data->philo[data->nb_philo - 1];
+	if (data->must_eat)
+	{
+		if (philo->nb_eat == data->must_eat)
+		{
+			data->full = true;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * For each philo,
+	checks if the time since the last meal as exceeded time_to_die
+*/
+int	check_death(void)
+{
+	int		time_check;
+	int		i;
+	t_philo	*philo;
+	t_data	*data;
+
+	i = 0;
+	data = call_struct();
+	pthread_mutex_lock(&data->mutex.death_mutex);
+	while (i < data->nb_philo)
+	{
+		philo = &data->philo[i];
+		time_check = time_stamp() - philo->time_last_meal;
+		pthread_mutex_unlock(&data->mutex.death_mutex);
+		if (time_check >= data->time_to_die)
+		{
+			print_action(philo->philo_id, time_stamp(), PHILO_DEAD);
+			pthread_mutex_lock(&data->mutex.change_state);
+			data->dead = true;
+			pthread_mutex_unlock(&data->mutex.change_state);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * Waits for each of the threads to be completed
+*/
+void	wait_thread(void)
+{
+	t_philo	*philo;
+	t_data	*data;
+	int		i;
+
+	i = 0;
+	data = call_struct();
+	while (i < data->nb_philo)
+	{
+		philo = &data->philo[i];
+		pthread_join(philo->thread_id, NULL);
+		i++;
+	}
+}
+
+/**
+ * Creates and launches all of the threads
+*/
+void	launcher(void)
+{
+	int i;
+	t_data *data;
+	t_philo *philo;
+
+	i = 0;
+	data = call_struct();
+	data->start_time = time_stamp();
+	while (i < data->nb_philo)
+	{
+		philo = &data->philo[i];
+		philo->time_last_meal = time_stamp();
+		pthread_create(&philo->thread_id, NULL, routine, philo);
+		i++;
+	}
+	check_death();
+	check_full();
+	wait_thread();
+	destroy_mutex();
+}
